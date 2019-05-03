73270eb38211b5876313634722930c91179f5ebc
y-horie517 <y.horie0517@gmail.com>
2019-05-03 13:58:29 +0900

    [Create]userのCRUD

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,49 @@
+class Admin::UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_users_path, notice: "ユーザを「#{@user.name}」登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "ユーザ「#{@user.name}」を更新しました。"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザを「#{@user.name}」削除しました。"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+end
