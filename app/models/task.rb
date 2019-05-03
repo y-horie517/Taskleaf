f7aa17974c377b01b9d4edab97747db7b9df4f80
yasuaki horie <yhorie@yasuakinoMacBook-Air.local>
2019-05-03 12:09:09 +0900

    [Add]バリデーション

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,18 @@
 class Task < ApplicationRecord
+    # 検証前に名前が入っていない場合の処理を実行
+    # before_validation :set_nameless_name
+
+    validates :name, presence: true
+    validates :name, length: { maximum: 30}
+    validate :validate_name_not_including_commma
+
+
+    private
+    def validate_name_not_including_commma
+        errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+    end
+
+    def set_nameless_name
+        self.name = '名前なし' if name.blank?
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_03_022510) do
+ActiveRecord::Schema.define(version: 2019_05_03_023524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tasks_on_name", unique: true
   end
 
 end
