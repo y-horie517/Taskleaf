2c87b91494e62a4de6f767cde69525ed24cb95ef
yasuaki horie <yhorie@yasuakinoMacBook-Air.local>
2019-05-03 11:36:44 +0900

    [Alter]nameに一意性制約追加

--- db/migrate/20190503022947_change_tasks_name_limit.rb
+++ db/migrate/20190503022947_change_tasks_name_limit.rb
@@ -3,6 +3,7 @@ class ChangeTasksNameLimit < ActiveRecord::Migration[5.2]
     change_column :tasks, :name, :string, limit: 30
   end
 
+  # 取り消しの際にはこちらが実行され、制約が取れる
   def down
     change_column :tasks, :name, :string
   end
