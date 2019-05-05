56d8ea08f1bfc7d55b366e5101636f2b6206e27a
y-horie517 <y.horie0517@gmail.com>
2019-05-05 16:53:36 +0900

    [Create]ログイン画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  
   namespace :admin do
     resources :users
   end
-  
+
 	root to: 'tasks#index'
 	resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
