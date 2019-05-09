1a405c6dc5b95dd3d3302990e60621a0db058d3c
y-horie517 <y.horie0517@gmail.com>
2019-05-09 13:03:32 +0900

    [Create]画像投稿機能

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,8 @@
 class Task < ApplicationRecord
+    has_one_attached :image
+
     # 検証前に名前が入っていない場合の処理を実行
     # before_validation :set_nameless_name
-
     validates :name, presence: true
     validates :name, length: { maximum: 30}
     validate :validate_name_not_including_commma
